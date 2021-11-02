ab9a86e73a423863d81dcd2df69d8eacac579b11
amaterasu303 <amatistaariza@unicesar.edu.co>
2021-11-02 03:47:27 -0500

    yhy

--- Proyectoadith/Form1.Designer.cs
+++ Proyectoadith/Form1.Designer.cs
@@ -174,7 +174,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(536, 158);
+            this.button2.Location = new System.Drawing.Point(531, 165);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(58, 33);
             this.button2.TabIndex = 13;
